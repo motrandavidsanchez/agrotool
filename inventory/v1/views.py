a5b9a985eb98fc78a81693c61d978a37c290cfd3
David Sanchez Motran <davidsanchezmotran@gmail.com>
2023-11-09 18:43:30 -0300

    CHANGE endpoints

--- inventory/v1/views.py
+++ inventory/v1/views.py
@@ -1,26 +1,26 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView
 
 from inventory.models import Tool, StaffEquipment, Supplies
 from inventory.v1.serializers import ToolSerializer, StaffEquipmentSerializer, SuppliesSerializer
 
 
-class ToolViewSet(ListAPIView, viewsets.GenericViewSet):
+class ToolViewSet(ListCreateAPIView, viewsets.GenericViewSet):
     queryset = Tool.objects.all()
     serializer_class = ToolSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['name', 'codigo', 'state']
 
 
-class StaffEquipmentViewSet(ListAPIView, viewsets.GenericViewSet):
+class StaffEquipmentViewSet(ListCreateAPIView, viewsets.GenericViewSet):
     queryset = StaffEquipment.objects.all()
     serializer_class = StaffEquipmentSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['name', 'codigo']
 
 
-class SuppliesViewSet(ListAPIView, viewsets.GenericViewSet):
+class SuppliesViewSet(ListCreateAPIView, viewsets.GenericViewSet):
     queryset = Supplies.objects.all()
     serializer_class = SuppliesSerializer
     filter_backends = [DjangoFilterBackend]
--- staff/v1/views.py
+++ staff/v1/views.py
@@ -1,10 +1,10 @@
 from rest_framework import viewsets
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView
 
 from staff.models import Staff
 from staff.v1.serializers import StaffSerializer
 
 
-class StaffViewSet(ListAPIView, viewsets.GenericViewSet):
+class StaffViewSet(ListCreateAPIView, viewsets.GenericViewSet):
     queryset = Staff.objects.all()
     serializer_class = StaffSerializer
