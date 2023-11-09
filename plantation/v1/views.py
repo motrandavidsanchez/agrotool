a5ee455ef6a92f8bf29d224da64a80ed8b1de3f8
David Sanchez Motran <davidsanchezmotran@gmail.com>
2023-11-09 18:42:45 -0300

    ADD endpoint para poner fecha de poda en lote

--- plantation/v1/views.py
+++ plantation/v1/views.py
@@ -4,28 +4,28 @@ from dateutil.relativedelta import relativedelta
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 
 from plantation.models import Hectare, Batch
 from plantation.v1.serializers import HectareSerializer, BatchSerializer
 
 
-class HectareViewSet(ListAPIView, viewsets.GenericViewSet):
+class HectareViewSet(ListCreateAPIView, viewsets.GenericViewSet):
     queryset = Hectare.objects.all()
     serializer_class = HectareSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['code']
 
 
-class BatchViewSet(ListAPIView, viewsets.GenericViewSet):
+class BatchViewSet(ListCreateAPIView, viewsets.GenericViewSet):
     queryset = Batch.objects.all()
     serializer_class = BatchSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['code', 'species', 'objetive', 'hectare']
+    filterset_fields = ['code', 'species', 'objetive', 'hectare', 'planting_date']
 
     @action(methods=['get'], detail=True)
-    def prune_batch(self, request, pk):
+    def get_days_prune_batch(self, request, pk):
         """
         Endpoint para calcular cuando dias faltan para la poda de Lote
         """
@@ -40,6 +40,20 @@ class BatchViewSet(ListAPIView, viewsets.GenericViewSet):
 
         return Response(
             data={'message': f'Faltan {pruning_days.days} dias para la poda del Lote.'},
+            status=status.HTTP_200_OK
+        )
+
+    @action(methods=["get"], detail=True)
+    def prune_batch(self, request, pk):
+        """
+        endpoint para colocar fecha de poda de lote
+        """
+        batch = self.get_object()
+        batch.pruning_date = datetime.date.today()
+        batch.save()
+
+        return Response(
+            data={"message": f"Poda del ({batch}) exitosa."},
             status=status.HTTP_200_OK
         )
 
